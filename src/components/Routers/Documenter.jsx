345626ca37ebb35afeac2f767d4cb8386c3ddaf6
Anna Rocha <annaleletr@gmail.com>
2023-08-19 20:39:43 -0300

    Format document

--- src/components/Routers/Documenter.jsx
+++ src/components/Routers/Documenter.jsx
@@ -2,45 +2,59 @@ import './css/Documenter.css';
 import React, { useRef } from 'react';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
-import dados from '../../Files/roteiro-is.json';
+import jsonData from '../../Files/roteiro-is.json';
 
 const Documenter = () => {
   const tableRef = useRef(null);
 
+  const calculatePosition = (index, campos) => {
+    let startPos = 1;
+    for (let i = 0; i < index; i++) {
+      startPos += campos[i].n_caracteres;
+    }
+    const endPos = startPos + campos[index].n_caracteres - 1;
+    return `${startPos.toString().padStart(3, '0')} - ${endPos.toString().padStart(3, '0')}`;
+  };
+
   const exportToPDF = () => {
     const doc = new jsPDF();
 
-    Object.keys(dados).forEach((roteiroKey) => {
-      const roteiro = dados[roteiroKey];
+    Object.keys(jsonData).forEach((roteiroKey) => {
+      const roteiro = jsonData[roteiroKey];
 
       doc.text(`${roteiroKey}`, 10, 10);
 
-      const table = document.createElement("table");
-      const thead = document.createElement("thead");
-      const tbody = document.createElement("tbody");
+      const table = document.createElement('table');
+      const thead = document.createElement('thead');
+      const tbody = document.createElement('tbody');
 
-      const headRow = document.createElement("tr");
-      ["Campo", "Número de Caracteres", "Posição", "Observação"].forEach((headText) => {
-        const th = document.createElement("th");
+      const headRow = document.createElement('tr');
+      ['Campo', 'Número de Caracteres', 'Observação', 'Posição'].forEach((headText) => {
+        const th = document.createElement('th');
         th.textContent = headText;
         headRow.appendChild(th);
       });
       thead.appendChild(headRow);
 
-      roteiro.forEach((item) => {
-        const bodyRow = document.createElement("tr");
-        ["campo", "n_caracteres", "posicao", "observacao"].forEach((field) => {
-          const td = document.createElement("td");
+      roteiro.forEach((item, index) => {
+        const bodyRow = document.createElement('tr');
+        ['campo', 'n_caracteres', 'observacao'].forEach((field) => {
+          const td = document.createElement('td');
           td.textContent = item[field];
           bodyRow.appendChild(td);
         });
+
+        const tdPosicao = document.createElement('td');
+        tdPosicao.textContent = calculatePosition(index, roteiro);
+        bodyRow.appendChild(tdPosicao);
+
         tbody.appendChild(bodyRow);
       });
 
       table.appendChild(thead);
       table.appendChild(tbody);
 
-      doc.autoTable({ html : table});
+      doc.autoTable({ html: table });
 
       doc.addPage();
     });
@@ -50,8 +64,8 @@ const Documenter = () => {
 
   return (
     <body>
-      {Object.keys(dados).map((roteiroKey) => {
-        const roteiro = dados[roteiroKey];
+      {Object.keys(jsonData).map((roteiroKey) => {
+        const roteiro = jsonData[roteiroKey];
         return (
           <div key={roteiroKey} className="table">
             <h2> {roteiroKey}</h2>
