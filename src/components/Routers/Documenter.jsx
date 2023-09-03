a37620362efa47e853924fd7be5d1dbc5253526f
Anna Rocha <annaleletr@gmail.com>
2023-09-03 20:31:50 -0300

    Edit ocumenter

--- src/components/Routers/Documenter.jsx
+++ src/components/Routers/Documenter.jsx
@@ -1,28 +1,21 @@
 import './css/Documenter.css';
-import React, { useRef } from 'react';
+import React, { useRef, Fragment } from 'react';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
 import jsonData from '../../Files/roteiro-is.json';
+import ReactDOMServer from 'react-dom/server';
 
 const Documenter = () => {
   const tableRef = useRef(null);
-
-  const calculatePosition = (index, campos) => {
-    let startPos = 1;
-    for (let i = 0; i < index; i++) {
-      startPos += campos[i].n_caracteres;
-    }
-    const endPos = startPos + campos[index].n_caracteres - 1;
-    return `${startPos.toString().padStart(3, '0')} - ${endPos.toString().padStart(3, '0')}`;
-  };
+  const ordemDasChaves = ["00", "A0", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "15Q", "16"];
 
   const exportToPDF = () => {
     const doc = new jsPDF();
 
-    Object.keys(jsonData).forEach((roteiroKey) => {
+    ordemDasChaves.forEach((roteiroKey) => {
       const roteiro = jsonData[roteiroKey];
 
-      doc.text(`${roteiroKey}`, 10, 10);
+      doc.text("Registro " + `${roteiroKey}`, 10, 10);
 
       const table = document.createElement('table');
       const thead = document.createElement('thead');
@@ -38,16 +31,23 @@ const Documenter = () => {
 
       roteiro.forEach((item, index) => {
         const bodyRow = document.createElement('tr');
+      
         ['campo', 'n_caracteres', 'observacao'].forEach((field) => {
           const td = document.createElement('td');
-          td.textContent = item[field];
+          if (field === 'observacao') {
+            const lines = item[field].split('\n'); 
+            const observacaoText = renderLines(lines);
+            td.innerHTML = ReactDOMServer.renderToStaticMarkup(observacaoText);
+          } else {
+            td.textContent = item[field];
+          }
           bodyRow.appendChild(td);
         });
-
+      
         const tdPosicao = document.createElement('td');
         tdPosicao.textContent = calculatePosition(index, roteiro);
         bodyRow.appendChild(tdPosicao);
-
+      
         tbody.appendChild(bodyRow);
       });
 
@@ -62,14 +62,32 @@ const Documenter = () => {
     doc.save('table.pdf');
   };
 
+  const calculatePosition = (index, campos) => {
+    let startPos = 1;
+    for (let i = 0; i < index; i++) {
+      startPos += campos[i].n_caracteres;
+    }
+    const endPos = startPos + campos[index].n_caracteres - 1;
+    return `${startPos.toString().padStart(3, '0')} - ${endPos.toString().padStart(3, '0')}`;
+  };
+
+  const renderLines = (lines) => {
+    return lines.map((line, index) => (
+      <Fragment key={index}>
+        {line}
+        {index < lines.length - 1 ? <br /> : null}
+      </Fragment>
+    ));
+  };
+
   return (
-    <body>
-      {Object.keys(jsonData).map((roteiroKey) => {
+    <div>
+      {ordemDasChaves.map((roteiroKey) => {
         const roteiro = jsonData[roteiroKey];
         return (
           <div key={roteiroKey} className="table">
-            <h2> {roteiroKey}</h2>
-            <table className="table" ref={tableRef}>
+            <h2>{roteiroKey}</h2>
+            <table className="table">
               <thead>
                 <tr>
                   <th>Campo</th>
@@ -83,8 +101,8 @@ const Documenter = () => {
                   <tr key={index}>
                     <td>{item.campo}</td>
                     <td>{item.n_caracteres}</td>
-                    <td>{item.posicao}</td>
-                    <td>{item.observacao}</td>
+                    <td>{calculatePosition(index, roteiro)}</td>
+                    <td>{renderLines(item.observacao.split('\n'))}</td>
                   </tr>
                 ))}
               </tbody>
@@ -92,10 +110,10 @@ const Documenter = () => {
           </div>
         );
       })}
-      <button className="btn btn-primary" onClick={exportToPDF}>
+      <button className="btn btn-primary floating-button" onClick={exportToPDF}>
         Exportar para PDF
       </button>
-    </body>
+    </div>
   );
 };
 
